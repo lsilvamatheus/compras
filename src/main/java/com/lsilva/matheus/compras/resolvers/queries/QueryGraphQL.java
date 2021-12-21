2823d8006baa9260469a7c9378b5bb8918ccd536
lsilvamatheus <lsilva.matheus@gmail.com>
2021-12-21 01:29:11 -0300

    Creating the Client query test method on QueryGraphQL endpoint

--- src/main/java/com/lsilva/matheus/compras/resolvers/queries/QueryGraphQL.java
+++ src/main/java/com/lsilva/matheus/compras/resolvers/queries/QueryGraphQL.java
@@ -1,6 +1,7 @@
 package com.lsilva.matheus.compras.resolvers.queries;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.lsilva.matheus.compras.entities.Client;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,5 +9,9 @@ public class QueryGraphQL  implements GraphQLQueryResolver {
 
     public String queryTest(){
         return "Query test running ok!";
+    }
+
+    public Client clientQueryTest(){
+        return new Client("Matheus Silva","matheus.silva@domain.com");
     }
 }
