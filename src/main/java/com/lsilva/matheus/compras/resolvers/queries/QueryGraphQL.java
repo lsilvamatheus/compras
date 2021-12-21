5ae09df573d7ef8898bd62088faa19e481fed7c5
lsilvamatheus <lsilva.matheus@gmail.com>
2021-12-21 02:19:26 -0300

    Adding the list and find by id methods on endpoint

--- src/main/java/com/lsilva/matheus/compras/resolvers/queries/QueryGraphQL.java
+++ src/main/java/com/lsilva/matheus/compras/resolvers/queries/QueryGraphQL.java
@@ -2,10 +2,20 @@ package com.lsilva.matheus.compras.resolvers.queries;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.lsilva.matheus.compras.entities.Client;
+import com.lsilva.matheus.compras.utils.ClientTestUtils;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class QueryGraphQL  implements GraphQLQueryResolver {
+
+    private final ClientTestUtils clientTestUtils;
+
+    public QueryGraphQL(ClientTestUtils clientTestUtils) {
+        this.clientTestUtils = clientTestUtils;
+    }
+
 
     public String queryTest(){
         return "Query test running ok!";
@@ -13,5 +23,13 @@ public class QueryGraphQL  implements GraphQLQueryResolver {
 
     public Client clientQueryTest(){
         return new Client("Matheus Silva","matheus.silva@domain.com");
+    }
+
+    public List<Client> clientListQueryTest(){
+        return clientTestUtils.buildClientListForTest();
+    }
+
+    public Client findClientByNameQueryTest(String name){
+        return clientTestUtils.findClientForTestByName(name);
     }
 }
